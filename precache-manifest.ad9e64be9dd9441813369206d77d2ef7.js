95946b5a5290e1f6b23814dfc3580b6529734730
Luís Carvalho <lapc19@hotmail.com>
2019-03-10 02:50:05 +0000

    Updates

---
+++ precache-manifest.ad9e64be9dd9441813369206d77d2ef7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/GameOfLife/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "741bf6f42b2ebfbf1173",
+    "url": "/GameOfLife/static/js/runtime~main.741bf6f4.js"
+  },
+  {
+    "revision": "4b0f5f248511b3d3ca05",
+    "url": "/GameOfLife/static/js/main.4b0f5f24.chunk.js"
+  },
+  {
+    "revision": "0fdafbb072dc2f72bb7b",
+    "url": "/GameOfLife/static/js/2.0fdafbb0.chunk.js"
+  },
+  {
+    "revision": "4b0f5f248511b3d3ca05",
+    "url": "/GameOfLife/static/css/main.c65d3ba8.chunk.css"
+  },
+  {
+    "revision": "8d12057e5b55461ccefc08bd94fae940",
+    "url": "/GameOfLife/index.html"
+  }
+];
