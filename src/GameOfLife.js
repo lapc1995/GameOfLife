d9c4a7c60192125977b916c94442da9e62823603
Luís Carvalho <lapc19@hotmail.com>
2019-03-12 18:18:32 +0000

    UI and running

--- src/GameOfLife.js
+++ src/GameOfLife.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Stage, Layer, Rect } from "react-konva";
+import { Stage, Layer, Rect, Line } from "react-konva";
 
 class GameOfLife extends React.Component {
     constructor(props) {
@@ -8,6 +8,7 @@ class GameOfLife extends React.Component {
             map: null,
             rectangles: [],
             running: false,
+            grid: false
         }
         this.map1 = this.generateRandomMap();
         this.map2 = this.generateEmptyMap();
@@ -16,28 +17,110 @@ class GameOfLife extends React.Component {
         this.runGame = this.runGame.bind(this);
         this.generateEmptyMapAction = this.generateEmptyMapAction.bind(this);
         this.generateRandomMapAction = this.generateRandomMapAction.bind(this);
-       // this.onMouseMove = this.onMouseMove.bind(this);
+        this.onClick = this.onClick.bind(this);
+        this.showGrid = this.showGrid.bind(this);
+
+        this.grid = this.createGrid();
     }
 
-    onMouseMove(e) {
-        console.log(this)
-        const position = this.getBoundingClientRect();
-        console.log(position, e.nativeEvent.offsetX, e.screenX);
-      }
+    createGrid() {
+    var lines = [];
+        for (var i = 0; i < window.innerWidth; i++) {
+            lines.push(<Line
+                points={[Math.round(i * 10) + 0.5, 0,  Math.round(i * 10) + 0.5, window.innerHeight]}
+                stroke="#ddd"
+                strokeWidth={1}
+                />);
+        }
+        for (var j = 0; j < window.innerHeight; j++) {
+            lines.push(<Line
+                points={[0, Math.round(j * 10),  window.innerWidth, Math.round(j * 10)]}
+                stroke="#ddd"
+                strokeWidth={0.5}
+                />);
+        }
+        return lines;
+    }
+
+    onClick(e) {
+        console.log(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+        this.setMapPosition(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+        
+    }
+
+    setMapPosition(x, y) {
+        let new_map;
+        let new_rectangles = [];
+        if(this.currentMap === 1 )  {
+            new_map = this.map2;
+            this.currentMap = 2;
+        } else {
+            new_map = this.map1;
+            this.currentMap = 1;
+        }
+        for(var i = 0; i < this.props.y; i++) {
+            for(var j = 0; j < this.props.x; j++) {
+                if(Math.floor(y/10) === i && Math.floor(x/10) === j){
+                    if(this.state.map[i][j] === 0)
+                        new_map[i][j] = 1;
+                    else
+                        new_map[i][j] = 0;
+                } else {
+                    new_map[i][j] = this.state.map[i][j];
+                }
+                if(new_map[i][j] === 1) 
+                    new_rectangles.push(
+                        <Rect
+                            x={j * 10}
+                            y={i * 10}
+                            width={10}
+                            height={10}
+                            fill={"rgb(0,0,0)"}
+                        />
+                    );
+            }
+        }
+        this.setState({
+            map: new_map,
+            rectangles: new_rectangles
+        });
+    }
+
 
     render() {
+        var grid = this.state.grid ? this.grid : [];
         return  <div>
-                    <div><RenderGame game={this.state.rectangles} t={this.onMouseMove}/></div>
+                    <div onClick={this.onClick} > <RenderGame game={this.state.rectangles} t={this.onMouseMove} grid={grid}/></div>
                     <div><ControlPanel runGame={this.runGame} 
                                        state={this.state.running?"Pause":"Play"}
                                        empty={this.generateEmptyMapAction}
-                                       random={this.generateRandomMapAction}/></div>
+                                       random={this.generateRandomMapAction}
+                                       grid={this.showGrid}/></div>
                 </div>
     }
 
     componentDidMount() {
+
+        var new_rectangles = [];
+        for(var i = 0; i < this.props.y; i++) {
+            for(var j = 0; j < this.props.x; j++) {
+                if(this.map1[i][j] === 1) 
+                    new_rectangles.push(
+                        <Rect
+                            x={j * 10}
+                            y={i * 10}
+                            width={10}
+                            height={10}
+                            fill={"rgb(0,0,0)"}
+                        />
+                    );
+            }
+        }
+
+
         this.setState({
-            map: this.map1
+            map: this.map1,
+            rectangles: new_rectangles
         });
 
         this.intervalID = setInterval(
@@ -46,8 +129,6 @@ class GameOfLife extends React.Component {
         );
 
         document.addEventListener("keydown", this.onKeyPressed, false);
-        //document.addEventListener("mousemove", this.onMouseMove, false);
-
     }
 
     componentWillUnmount() {
@@ -158,6 +239,12 @@ class GameOfLife extends React.Component {
         });
     }
 
+    showGrid() {
+        this.setState({
+            grid: !this.state.grid
+        });
+    }
+
     generateRandomMapAction() {
         if(this.currentMap === 1) {
             this.map2 = this.generateRandomMap();
@@ -172,6 +259,26 @@ class GameOfLife extends React.Component {
                 map: this.map1
             });
         }
+
+        var new_rectangles = [];
+        for(var i = 0; i < this.props.y; i++) {
+            for(var j = 0; j < this.props.x; j++) {
+                if(this.state.map[i][j] === 1) 
+                    new_rectangles.push(
+                        <Rect
+                            x={j * 10}
+                            y={i * 10}
+                            width={10}
+                            height={10}
+                            fill={"rgb(0,0,0)"}
+                        />
+                    );
+            }
+        }
+
+        this.setState({
+            rectangles: new_rectangles
+        });
     }
 
     generateEmptyMapAction() {
@@ -188,6 +295,10 @@ class GameOfLife extends React.Component {
                 map: this.map1
             });
         }
+
+        this.setState({
+            rectangles: []
+        });
     }
 }
 
@@ -196,24 +307,24 @@ return  <div>
             <button onClick={props.runGame}> {props.state} </button>
             <button onClick={props.empty}> New Empty Map </button>
             <button onClick={props.random}> New Random Map </button>
+            <button onClick={props.grid}> Grid </button>
         </div>
 
 
 
 }
 
-class RenderGame extends React.Component {
-        constructor(props) {
-            super(props);
-            }
-    render() {
-    return(
-    <div onMouseMove={this.props.t}>
-    <Stage  width={window.innerWidth} height={window.innerHeight}  >
-                <Layer>
-                    {this.props.game}
-                </Layer>
-    </Stage></div>)}
+function RenderGame(props) {
+    return( <div>
+                <Stage  width={window.innerWidth} height={window.innerHeight}  >
+                    <Layer>
+                        {props.game}
+                    </Layer>
+                    <Layer>
+                        {props.grid}
+                    </Layer>
+                </Stage>
+            </div>)
 }
 
 export default GameOfLife;
